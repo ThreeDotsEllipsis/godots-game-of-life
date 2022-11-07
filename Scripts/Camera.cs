1f12229c352c7667aeaabd5a0e88ce866d716652
threedots <threedotsellipsis@protonmail.com>
2022-11-07 20:44:18 +0500

    Quick camera done

---
+++ Scripts/Camera.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class Camera : Camera2D
+{
+    [Export]
+    float moveSpeed = 5f;
+
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+
+        float x = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+        float y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
+
+        this.Position += new Vector2(x, y) * moveSpeed;
+    }
+
+}
