1e496316dd6605b2f0f5e09c3d6199bee4cb2853
threedots <threedotsellipsis@protonmail.com>
2022-11-07 20:33:05 +0500

    A better more efficient implementation

--- Scripts/Cells.cs
+++ Scripts/Cells.cs
@@ -5,78 +5,48 @@ public class Cells : TileMap
 {
     private Godot.Collections.Array usedCells;
 
-    public override void _Ready()
+    private Godot.Collections.Array GetEmptyNeighbourCells(Vector2 cell)
     {
-        base._Ready();
-        GD.Print(this.GetUsedCells());
+        var emptyCells = new Godot.Collections.Array();
+
+        for (int x = (int)cell.x - 1; x <= cell.x + 1; x++)
+        {
+            for (int y = (int)cell.y - 1; y <= cell.y + 1; y++)
+            {
+                var coords = new Vector2(x, y);
+
+                if (!usedCells.Contains(coords))
+                {
+                    emptyCells.Add(coords);
+                }
+            }
+        }
+
+        return emptyCells;
     }
 
     public void NextTurn()
     {
         usedCells = this.GetUsedCells();
-        this.CreateCells();
-        this.DestroyCells();
-    }
 
-    private void CreateCells()
-    {
         foreach (Vector2 cell in usedCells)
         {
-            for (int x = (int)cell.x - 1; x <= cell.x + 1; x++)
-            {
-                for (int y = (int)cell.y - 1; y <= cell.y + 1; y++)
-                {
+            var emptyCells = GetEmptyNeighbourCells(cell);
+            int numOfAliveCells = 8 - emptyCells.Count;
 
-
-                    int numOfAliveCells = 0;
-
-                    for (int x1 = x - 1; x1 <= x + 1; x1++)
-                    {
-                        for (int y1 = y - 1; y1 <= y + 1; y1++)
-                        {
-                            if (usedCells.Contains(new Vector2(x1, y1)))
-                            {
-                                numOfAliveCells++;
-                            }
-                        }
-                    }
-
-                    if (numOfAliveCells == 3)
-                    {
-                        this.SetCell(x, y, 0);
-                    }
-
-
-                }
-            }
-        }
-    }
-
-    private void DestroyCells()
-    {
-        foreach (Vector2 cell in usedCells)
-        {
-            int numOfAliveCells = -1;
-
-            for (int x = (int)cell.x - 1; x <= cell.x + 1; x++)
-            {
-                for (int y = (int)cell.y - 1; y <= cell.y + 1; y++)
-                {
-                    if (usedCells.Contains(new Vector2(x, y)))
-                    {
-                        numOfAliveCells++;
-                    }
-                }
-            }
-
-            if (numOfAliveCells < 2)
+            if (numOfAliveCells < 2 || numOfAliveCells > 3)
             {
                 this.SetCell((int)cell.x, (int)cell.y, -1);
             }
 
-            if (numOfAliveCells > 3)
+            foreach (Vector2 emptyCell in emptyCells)
             {
-                this.SetCell((int)cell.x, (int)cell.y, -1);
+                numOfAliveCells = 9 - GetEmptyNeighbourCells(emptyCell).Count;
+
+                if (numOfAliveCells == 3)
+                {
+                    this.SetCell((int)emptyCell.x, (int)emptyCell.y, 0);
+                }
             }
         }
     }
--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,13 +3,16 @@ using System;
 
 public class Main : Node2D
 {
+    Cells cells;
+
     public override void _Ready()
     {
+        cells = GetNode<Cells>("Cells");
     }
 
     private void _on_TickTimer_timeout()
     {
-        GetNode<Cells>("Cells").NextTurn();
+        cells.NextTurn();
     }
 }
 
