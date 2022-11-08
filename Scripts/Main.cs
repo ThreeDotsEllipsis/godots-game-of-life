7f5b3080809afc804e672075a5b1c4c37cfe1963
threedots <threedotsellipsis@protonmail.com>
2022-11-08 19:45:28 +0500

    Fixed add / remove ability

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -31,9 +31,21 @@ public class Main : Node2D
 
             if (Input.IsMouseButtonPressed((int)ButtonList.Left))
             {
-                cells.SetCell((int)hintSprite.Position.x / 64, (int)hintSprite.Position.y / 64, 0);
+                cells.SetCell((int)cellPosition.x, (int)cellPosition.y, 0);
             }
             else if (Input.IsMouseButtonPressed((int)ButtonList.Right))
+            {
+                cells.SetCell((int)cellPosition.x, (int)cellPosition.y, -1);
+            }
+        }
+
+        else if (ievent is InputEventMouseButton mbevent)
+        {
+            if (mbevent.ButtonIndex == (int)ButtonList.Left)
+            {
+                cells.SetCell((int)hintSprite.Position.x / 64, (int)hintSprite.Position.y / 64, 0);
+            }
+            else if (mbevent.ButtonIndex == (int)ButtonList.Right)
             {
                 cells.SetCell((int)hintSprite.Position.x / 64, (int)hintSprite.Position.y / 64, -1);
             }
